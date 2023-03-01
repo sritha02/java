5ace000694df90d94d5fa4fb766749fd0ea200da
sritha02 <87262592+sritha02@users.noreply.github.com>
2023-03-01 20:29:24 +0530

    Create getting length of a string.java

---
+++ getting length of a string.java
@@ -0,0 +1,7 @@
+class Main{
+  public static void main(String[] args)
+    String greet="Hello!World";
+  System.out.println("String:"+greet);
+  int length=greet.length();
+  System.out.println("Length"+length);
+}}
